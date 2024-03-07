efe31c1c2fc13dbbc3c0c1d4347513119bd9e2f4
Samuel260107 <samuel.americon02@gmail.com>
2024-03-07 08:19:37 -0300

    hahahaha

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <Div>
+    <form action="cadastrar.php">
+        <input name="nome">
+        <input name="curso">
+        <input name="rm">
+        <h1>
+        <input type="radio" name="masc">
+        <input type="radio" name="fem">
+        <button type="submit"></button>
+    </form>
+    <Div>
+</body>
+</html>
